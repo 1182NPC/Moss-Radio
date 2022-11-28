e2b4df7953271a962e80af53de68cee69aa40476
Nicholas Drömert <campbell.cnp@gmail.com>
2022-11-28 13:13:34 +0100

    setup migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_120003) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_120801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,33 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_120003) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "author"
+    t.datetime "date_published"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "radiosets", force: :cascade do |t|
+    t.datetime "date"
+    t.string "audio_link"
+    t.bigint "resident_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["resident_id"], name: "index_radiosets_on_resident_id"
+  end
+
+  create_table "residents", force: :cascade do |t|
+    t.text "bio"
+    t.string "links"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +73,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_120003) do
 
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "posts", "users"
+  add_foreign_key "radiosets", "residents"
 end
