adabf6809f09c4e87b2f0304e80a9a87c767884d
Nicholas Drömert <campbell.cnp@gmail.com>
2022-11-28 15:52:21 +0100

    MOrorore

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,5 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
   connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => console.log(data) }
+    )
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 }
