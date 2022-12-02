cc8385ce0978ff1ce94ddcf57b6ba545cbdde949
Nicholas Drömert <campbell.cnp@gmail.com>
2022-12-02 15:10:26 +0100

    help

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :postlikes, dependent: :destroy
   scope :published, -> { where(published: true) }
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
 end
