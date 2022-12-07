d4d36702fc2c6b21bfd566a36b2db201c78f078e
Nicholas Drömert <campbell.cnp@gmail.com>
2022-12-07 11:43:07 +0100

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,8 @@ somelinks = ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/
             "https://www.youtube.com/embed/KGwDl_0mAys",
             "https://www.youtube.com/watch?v=JK8ilaPZbKE&list=PLZUqUj66LOrF-KP4mhdmXszkKKvETyQap&index=3W"]
 
+sometitles = []
+
 puts 'Cleaning up database...'
 Post.destroy_all
 Radioset.destroy_all
@@ -36,14 +38,41 @@ end
 
 Chatroom.create(name: "Live Chat")
 
+seed_post = Post.create(title: "Music as a social experience in the digital age",
+                        rich_body: "<div class='trix-content'>
+                        <div>
+                        <strong>Music and mood<br><br></strong>Though music tends to travel via the collective experience,
+                        it remains an immensely personal aspect of our lives that has, for many of us, become inextricable from our memories,
+                        aspirations and identities. It forms a crucial part of the individual, acting as a window into how a person views themselves and their position in the world,
+                        through expression in music performance and through reflection in music listening. <br><br>Music and mood have a long-established and multifaceted relationship where mood can impact music listening decisions and music can express or induce emotional states:
+                        ideas which have been harnessed in some <a href='https://81e0ff5d-f7d2-4d6f-b039-16dad765f9ef.filesusr.com/ugd/5109df_5727b0d24fe342f8aaa594157317895d.pdf'>successful music therapies</a>. Musical elements like tempo,
+                        rhythm, timbre and harmony may be used to either reflect or counteract one's mood; for example, you may choose slower, more harmonic music to relax when feeling anxious, or choose faster,
+                        more upbeat music to maintain a positive mood. These kinds of <a href='https://81e0ff5d-f7d2-4d6f-b039-16dad765f9ef.filesusr.com/ugd/5109df_b49282768d2140de9d0ec6d5a2b978a8.pdf'>
+                        associations are cross-cultural</a> with a fairly universal understanding of cues like vocal intensity as an indication of anger.
+                        There are of course some more culturally specified representations of emotion in music, with minor keys and dissonance being more affiliated with sadness in Western music.
+                        <br><br><strong>Music in social spaces </strong><br><br><br>It's Friday night, and you're heading round to a gaff with your pals from art school. It's not your usual skeezy piss-up,
+                        it's one of those parties where all the boys have tiny beanies and all the girls have new wave mullets. It is, for all intents and purposes,
+                        a Cool Party. It's good fun, until you notice that the aux is working its way towards you. Christ. What will you put on? The fate of your interactions for the rest of your night rests upon this. <br><br>
+                        </div><div>
+                        <br>You pause, and then decide to take the risk. Fergie's <a href='https://open.spotify.com/track/5Nu1cp2yi4TlZF4KTmElFD'><em>Fergalicious</em></a> begins to play out the speaker and agonisingly,
+                        you wait for everyone to register what's been put on. A few excruciating seconds pass… and the party goes wild. Party-goers,
+                        regardless of their stylistic sub-genre – e-girls, skaters, punks, softbois, club kids and hip hop rats – start cutting shapes to Fergie. The queue is then quickly loaded with pop classics,
+                        <em>Sound of the Underground</em> by Girls Aloud, <em>Sweet Escape</em> by Gwen Stefani, <em>Can't Get You Out Of My Head</em> by Kylie Minogue… <br><br><br><em>This example blog was taken from </em><a href='https://texturemag.net/'><em>texturemag</em></a><em>.</em>
+                        </div><div>
+                        <br>
+                        </div>
+                        </div>", date_published: Faker::Date.backward, author: "Sandra Smith", user: User.all.sample, published: true)
 
+multi_path = File.join(Rails.root, 'app/assets/images/multi_image_seed')
+seed_post.photos.attach(io: File.open("#{multi_path}/multi_seed1.jpg"), filename: 'multi_seed1.jpg',
+                        content_type: 'image/jpg')
 
-20.times do
+19.times do
   Resident.create(name: Faker::Games::WarhammerFantasy.hero, bio: Faker::Games::WarhammerFantasy.quote, links: "#{Faker::Internet.url}", photo: Faker::Avatar.image)
 end
 
-20.times do
-  Post.create(title: Faker::Books::CultureSeries.culture_ship, rich_body: Faker::Lorem.paragraph(sentence_count: 100), date_published: Faker::Date.backward, author: Faker::Ancient.god, user: User.all.sample, published: true)
+50.times do
+  Post.create(title: sometitles.sample, rich_body: Faker::Lorem.paragraph(sentence_count: 100), date_published: Faker::Date.backward, author: Faker::Ancient.god, user: User.all.sample, published: true)
 end
 
 
@@ -57,3 +86,6 @@ end
 
 
 # https://api.unsplash.com/photos/random/?client_id=mNgjWTimcrM2nhflhnZn5GT-Wc_i-JmfjXewz33l_-4
+
+
+# USE COMPRESSED IMAGE FOR PRESENTATION
